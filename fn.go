2f37e022e6e9a0e3d4140f85544565f782873f18
Vetle Kise <96139038+vetlekise@users.noreply.github.com>
2026-04-20 13:17:12 +0200

    feat: Create function for creating Namespaces based on spec.environments

--- fn.go
+++ fn.go
@@ -7,9 +7,13 @@ import (
 	"github.com/crossplane/function-sdk-go/logging"
 	fnv1 "github.com/crossplane/function-sdk-go/proto/v1"
 	"github.com/crossplane/function-sdk-go/request"
+	"github.com/crossplane/function-sdk-go/resource"
+	"github.com/crossplane/function-sdk-go/resource/composed"
 	"github.com/crossplane/function-sdk-go/response"
+	"k8s.io/client-go/kubernetes/scheme"
 
-	"github.com/crossplane/function-sandbox-go/input/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Function returns whatever response you ask it to.
@@ -25,30 +29,53 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1.RunFunctionRequest) 
 
 	rsp := response.To(req, response.DefaultTTL)
 
-	in := &v1beta1.Input{}
-	if err := request.GetInput(req, in); err != nil {
-		// You can set a custom status condition on the claim. This allows you to
-		// communicate with the user. See the link below for status condition
-		// guidance.
-		// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-		response.ConditionFalse(rsp, "FunctionSuccess", "InternalError").
-			WithMessage("Something went wrong.").
-			TargetCompositeAndClaim()
-
-		// You can emit an event regarding the claim. This allows you to communicate
-		// with the user. Note that events should be used sparingly and are subject
-		// to throttling; see the issue below for more information.
-		// https://github.com/crossplane/crossplane/issues/5802
-		response.Warning(rsp, errors.New("something went wrong")).
-			TargetCompositeAndClaim()
-
-		response.Fatal(rsp, errors.Wrapf(err, "cannot get Function input from %T", req))
+	xr, err := request.GetObservedCompositeResource(req)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot get observed composite resource from %T", req))
 		return rsp, nil
 	}
 
-	// TODO: Add your Function logic here!
-	response.Normalf(rsp, "I was run with input %q!", in.Example)
-	f.log.Info("I was run!", "input", in.Example)
+	names, err := xr.Resource.GetStringArray("spec.environments")
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot read spec.environments field of %s", xr.Resource.GetKind()))
+		return rsp, nil
+	}
+	f.log.Info("Read environments from XR", "xr", xr.Resource.GetName(), "count", len(names), "environments", names)
+
+	desired, err := request.GetDesiredComposedResources(req)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot get desired resources from %T", req))
+		return rsp, nil
+	}
+
+	_ = scheme.AddToScheme(composed.Scheme) // registers all core k8s types
+
+	for _, env := range names {
+		f.log.Debug("Creating namespace for environment", "namespace", xr.Resource.GetName()+"-"+env)
+		ns := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: xr.Resource.GetName() + "-" + env,
+				Labels: map[string]string{
+					"managed-by":  "crossplane",
+					"environment": env,
+				},
+			},
+		}
+
+		cd, err := composed.From(ns)
+		if err != nil {
+			response.Fatal(rsp, errors.Wrapf(err, "cannot convert namespace %s", env))
+			return rsp, nil
+		}
+
+		desired[resource.Name("ns-"+env)] = &resource.DesiredComposed{Resource: cd}
+	}
+	f.log.Info("Set desired namespaces", "count", len(desired))
+
+	if err := response.SetDesiredComposedResources(rsp, desired); err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot set desired composed resources in %T", rsp))
+		return rsp, nil
+	}
 
 	// You can set a custom status condition on the claim. This allows you to
 	// communicate with the user. See the link below for status condition
--- input/v1beta1/input.go
+++ input/v1beta1/input.go
@@ -22,6 +22,6 @@ type Input struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// Example is an example field. Replace it with whatever input you need. :)
-	Example string `json:"example"`
+	// Environments is a list of strings that will be used to created Namespaces.
+	Environments []string `json:"environments"`
 }
